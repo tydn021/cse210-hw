5de68e0fc01ee021505be854638b04957d21cb27
tydn021 <tnelson9@byupathway.edu>
2026-01-06 19:23:59 -0800

    Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,7 +3,16 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+    // Ask user for their name.
+    
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }

7346e181c7876581578f2cf2d043b62f3c3d460a
tydn021 <tnelson9@byupathway.edu>
2026-01-13 20:37:26 -0800

    Activyt 1 done

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear= 2022;
+        job2._endYear= 2023;
+
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Tyler Nelson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
